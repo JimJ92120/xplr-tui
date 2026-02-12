e371d6ac9efb3e7ee8e1af46c91b2ec421bff97d
JimJ92120 <jimj.development@gmail.com>
2026-02-12 22:59:47 +0700

    feat(store > command > controller): create parent directories if not exists when moving or copying

--- src/store/command/controller.rs
+++ src/store/command/controller.rs
@@ -18,21 +18,34 @@ impl CommandController {
             panic!("'{}' already exists.", target_path_name);
         }
 
+        if let Some(target_parent_directory) = target_path.parent() {
+            fs::create_dir_all(target_parent_directory)?;
+        }
+
         match fs::copy(&source_path_name, &target_path_name) {
             Ok(_) => Ok(()),
             Err(error) => panic!("Unable to copy '{}' to '{}'\n{}", source_path_name, target_path_name, error), 
         }
     }
 
-    pub fn move_file_or_directory(source_path_name: String, new_name: String) -> Result<()> {
+    pub fn move_file_or_directory(source_path_name: String, target_path_name: String) -> Result<()> {
         let source_path = Path::new(&source_path_name);
         if !source_path.exists() {
             panic!("'{}' doesn't exist.", source_path_name);
         }
 
-        match fs::rename(&source_path_name, &new_name) {
+        let target_path = Path::new(&target_path_name);
+        if target_path.exists() {
+            panic!("'{}' already exists.", target_path_name);
+        }
+
+        if let Some(target_parent_directory) = target_path.parent() {
+            fs::create_dir_all(target_parent_directory)?;
+        }
+
+        match fs::rename(&source_path_name, &target_path_name) {
             Ok(_) => Ok(()),
-            Err(error) => panic!("Unable to move '{}' to '{}'.\n{}", source_path_name, new_name, error),
+            Err(error) => panic!("Unable to move '{}' to '{}'.\n{}", source_path_name, target_path_name, error),
         }
     }
 
